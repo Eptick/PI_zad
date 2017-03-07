4219514f8f6ad17e119cca85f48551e24cce5540
Eptick <redzicleon@gmail.com>
2017-03-07 19:17:43 +0100

    Kreiran i završen zadatak 5

---
+++ Vjezba_pdf_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Napravite konzolnu aplikaciju u kojoj korisnik unosi broj N.
+ * Aplikacija računa i ispisuje sumu prirodnih brojeva od 1 do N.
+ * 
+ * */
+namespace Vjezba_pdf_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj N: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            int suma = 0;
+            for (int i = 1; i < n; i++)
+            {
+                suma += i;
+            }
+            Console.WriteLine(suma);
+            Console.ReadLine();
+        }
+    }
+}
