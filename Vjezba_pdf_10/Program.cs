0987089c4ba4e931619ae5fa9a02b8996915ad49
Eptick <redzicleon@gmail.com>
2017-03-07 20:22:12 +0100

    Kreirani i riješeni zadatci 10 i 11

---
+++ Vjezba_pdf_10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Napravite konzolnu aplikaciju u kojoj korisnik unosi pet brojeva.
+ * aplikacija ispisuje unesene brojeve u jednom redu. u drugom redu ispisuje sumu i prosijek
+ * */
+namespace Vjezba_pdf_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Int16> lista = new List<Int16>();
+            lista.Add(Convert.ToInt16(Console.ReadLine()));
+            lista.Add(Convert.ToInt16(Console.ReadLine()));
+            lista.Add(Convert.ToInt16(Console.ReadLine()));
+            lista.Add(Convert.ToInt16(Console.ReadLine()));
+            lista.Add(Convert.ToInt16(Console.ReadLine()));
+            Console.Write(lista[0]);
+            Console.Write(" ");
+            Console.Write(lista[1]);
+            Console.Write(" ");
+            Console.Write(lista[2]);
+            Console.Write(" ");
+            Console.Write(lista[3]);
+            Console.Write(" ");
+            Console.Write(lista[4]);
+            Console.Write("\n");
+            int suma = 0, prosjek = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                suma += lista[i];
+            }
+            prosjek = suma / 5;
+            Console.Write(suma);
+            Console.Write(" ");
+            Console.Write(prosjek);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vjezba_pdf_11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Napravite konzolnu aplikaciju u kojoj korisnik unosi širinu i visinu prozora konzole.
+ * Nije moguće unesti vrijednosti manje od 1. Nakon unosa zadnje broja (visine) 
+ * veličina prozora konzole se postavi u zadane vrijednosti
+ * */
+namespace Vjezba_pdf_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s, v;
+            do
+            {
+                s = Convert.ToInt16(Console.ReadLine());
+            } while (s < 1);
+            do
+            {
+                v = Convert.ToInt16(Console.ReadLine());
+            } while (v < 1);
+
+            Console.WindowHeight = s;
+            Console.WindowWidth = v;
+
+            Console.Read();
+
+        }
+    }
+}
