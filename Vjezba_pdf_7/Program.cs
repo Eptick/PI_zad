4b1ce53213fce4dfa562e4033c87f090a3cc23b1
Eptick <redzicleon@gmail.com>
2017-03-07 19:53:48 +0100

    kreiran i riješen zadatak 7

---
+++ Vjezba_pdf_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Napravite konzolnu aplikaciju koja crta srce visine minimalno 5 redova
+ * i u zadanoj boji koju korisnik unese ("Crvena", "Plava")
+ * */
+namespace Vjezba_pdf_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string boja = Console.ReadLine();
+            switch (boja)
+            {
+                case "Crvena":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "Plava":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+            }
+            Console.Write(" °° °°\n°  °  °\n°     °\n °   °\n   °\n");
+            
+            Console.ReadLine();
+        }
+    }
+}
