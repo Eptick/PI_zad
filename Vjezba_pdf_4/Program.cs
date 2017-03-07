f3fe6c8b8c65bdd3653882abf72acace33e68af0
Eptick <redzicleon@gmail.com>
2017-03-07 19:14:33 +0100

    Kreiran i završen zadatak 4

---
+++ Vjezba_pdf_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Napravite konzolnu aplikaciju u kojoj korisnik unosi tekst i broj.
+ * Aplikacija ispisuje uneseni tekst broj puta.
+ * */
+namespace Vjezba_pdf_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite tekst: ");
+            String t = Console.ReadLine();
+            Console.WriteLine("Unesite broj: ");
+            int b = Convert.ToInt16(Console.ReadLine());
+            while (b>0)
+            {
+                Console.WriteLine(t);
+                b--;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
