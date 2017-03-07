c144ddf6eaf8110142372fefcaebe2bbf2591687
Eptick <redzicleon@gmail.com>
2017-03-07 19:09:52 +0100

    Gotov Zadatak 3

--- Vjezba_pdf_3/Program.cs
+++ Vjezba_pdf_3/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Napravite konzolnu aplikaciju u kojoj korisnik unosi dva broja i 
+ * operator (+,-,*,/), aplikacija ispisuje rezultat matematičke operacije
+ * */
 namespace Vjezba_pdf_3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int prvi = Convert.ToInt16(Console.ReadLine());
+            int drugi = Convert.ToInt16(Console.ReadLine());
+
+            string op = Console.ReadLine();
+            float r;
+            switch (op[0])
+            {
+                case  '+':
+                    r = prvi + drugi;
+                    break;
+                case '-':
+                    r = prvi - drugi;
+                    break;
+                case '/':
+                    r = prvi / drugi;
+                    break;
+                case '*':
+                    r = prvi * drugi;
+                    break;
+                default:
+                    r = 0;
+                    break;
+            }
+            Console.WriteLine(r);
+            Console.ReadLine();
+
         }
     }
 }
