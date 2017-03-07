bb3ab30a8241329321700aeb78e2d3ac5ff21d78
Eptick <redzicleon@gmail.com>
2017-03-07 16:22:40 +0100

    Gotov zadatak 2

--- Vjezba_pdf_2/Program.cs
+++ Vjezba_pdf_2/Program.cs
@@ -10,6 +10,20 @@ namespace Vjezba_pdf_2
     {
         static void Main(string[] args)
         {
+            int broj;
+            do
+            {
+                Console.Write("Unesite jedinice: ");
+                int prvi = Convert.ToInt16(Console.ReadLine());
+
+                Console.Write("Unesite desetice: ");
+                int drugi = Convert.ToInt16(Console.ReadLine());
+                drugi = drugi * 10;
+
+                broj = prvi + drugi;
+            }  while (broj > 99);
+            Console.WriteLine(broj*2);
+            Console.ReadLine();
         }
     }
 }
