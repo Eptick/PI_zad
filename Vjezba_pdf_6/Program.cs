4b08a75670c150b42ab4f179bdc74aa316c653b6
Eptick <redzicleon@gmail.com>
2017-03-07 19:30:54 +0100

    Kreiran i riješen zadatak 6

---
+++ Vjezba_pdf_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+ * Napravite konzolnu aplikaciju u kojoj korisnik unosi tekst i boju.
+ * Aplikacija ispisuje tekst u zadanoj boji. Moguće boje su "Crvena", "Plava" i "Zelena"
+ *  */
+namespace Vjezba_pdf_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t = Console.ReadLine();
+            string c = Console.ReadLine();
+            switch (c)
+            {
+                case "Crvena":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "Plava":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "Zelena":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+            }
+            Console.WriteLine(t);
+            Console.ReadLine();
+        }
+    }
+}
